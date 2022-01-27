b349d8e24c4e82bc1daaefd7c2670870d1a7d6b5
JibronHarris <Jibron.Harris@gmail.com>
2022-01-27 15:50:19 -0500

    Added models and set up associations

--- db/index.js
+++ db/index.js
@@ -2,6 +2,58 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize("postgres://localhost:5432/musicschool");
 
+const Network = db.define("network", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+    channel: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+});
+
+const Show = db.define("show", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+    rating: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+});
+
+const Genre = db.define("genre", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+Network.hasMany(Show);
+
+Show.belongsToMany(Genre, { through: "show_genre" });
+Genre.belongsToMany(Show, { through: "show_genre" });
+
 module.exports = {
+  Network,
+  Show,
+  Genre,
   db,
 };
